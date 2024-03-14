c0649e1dd0c6b92b10095f4dbc9d92f350f3e4cb
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-03-14 07:35:24 -0700

    3rd task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     else:
-        print("{} arguments.".format(len(argv) - 1))
+        if len(argv) == 2:
+            print("1 argument.")
+        else:
+            print("{} arguments.".format(len(argv) - 1))
         for i, arg in enumerate(argv[1:]):
             print("{:d}: {}".format(i, arg))
