d58ccce8db8efeb226d813628e7365f03e5cf982
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-03-14 07:25:33 -0700

    3rd task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    if len(argv) == 1:
+        print("0 arguments.")
+    else:
+        for i, arg in enumerate(argv[1:]):
+            print("{:d}: {}".format(i, arg))
