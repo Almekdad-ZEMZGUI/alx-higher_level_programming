15069bbc0ce47717ed9df6541b8b38e11cd3a84c
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-03-16 05:17:00 -0700

    7th task

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    if matrix is None:
+        return None
     for row in matrix:
         for ele in row:
             [print("{:d}".format(ele), end=" " if ele != row[-1] else "\n")]
