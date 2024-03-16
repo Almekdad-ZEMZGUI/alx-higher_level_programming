24e80d3c8955a69354a4ebaf152fb496786e5da7
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-03-16 06:14:36 -0700

    14th task

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,33 +1,34 @@
 #include "lists.h"
 
 /**
-* is_palindrome - checks if a singly linked list is a palindrome
-* @head: double ptr to the head of linkedlist
-*
-* Return: 0 if it is not a palindrome, 1 if it is a palindrome
-*/
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @head: double ptr to the head of linkedlist
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
-        listint_t *temp = *head;
-        unsigned int length = 0, i;
-        int arr[10240];
-        
-        if (!*head)
-                return (1);
-        
-        for (i = 0; temp; i++)
-        {
-                arr[i] = temp->n;
-                temp = temp->next;
-                length++;
-        }
-        if (length == 1)
-                return (1);
+	listint_t *temp = *head;
+	unsigned int length = 0, i;
+	int arr[10240];
 
-        for (i = 0; i <= length / 2; i++)
-        {
-                if (arr[i] != arr[length - i - 1])
-                        return (0);
-        }
-        return (1);
+	if (!*head)
+		return (1);
+
+	for (i = 0; temp; i++)
+	{
+		arr[i] = temp->n;
+		temp = temp->next;
+		length++;
+	}
+	if (length == 1)
+		return (1);
+
+	for (i = 0; i <= length / 2; i++)
+	{
+		if (arr[i] != arr[length - i - 1])
+			return (0);
+	}
+	return (1);
 }
+
