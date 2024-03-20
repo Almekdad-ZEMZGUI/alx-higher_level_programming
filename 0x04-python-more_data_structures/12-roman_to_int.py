91516e29d26b8aff7c1410b3d3dc332d24f2fdf9
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-03-20 09:07:04 -0700

    13th task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return None
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    res = dic[roman_string[-1]]
+    for i in range(len(roman_string) - 1, 0, -1):
+        if dic[roman_string[i - 1]] < dic[roman_string[i]]:
+            res -= dic[roman_string[i - 1]]
+        else:
+            res += dic[roman_string[i - 1]]
+    return res
