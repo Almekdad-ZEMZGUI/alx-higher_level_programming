c2a8d4ee80cad144cb44ed8f55baa1b523a9e234
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-02 14:26:56 -0700

    3rd task

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for ele in my_list[:x]:
+    count = 0
+    for ele in my_list[:x]:
+        try:
             print("{:d}".format(ele), end="")
             count += 1
-        print()
-        return count
-    except (ValueError, TypeError):
-        print()
-        return count
+        except (ValueError, TypeError):
+            pass
+    print()
+    return count
