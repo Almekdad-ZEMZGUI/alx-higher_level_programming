6a1f1af261e6e5859ce794713bca45456255e2f0
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-02 17:21:24 -0700

    2nd task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """ Empty class defines a square """
-    def __init__(self, size):
+    def __init__(self, size=None):
         """The size of a square is crucial for a square,
         many things depend of it (area computation, etc.)"""
         self.size = size
