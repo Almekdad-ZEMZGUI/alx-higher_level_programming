0176682631e56656874c9ed7e21cad1b2b845b2e
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-02 17:38:43 -0700

    4th task

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" First task """
+
+
+class Square:
+    """ Empty class defines a square """
+    def __init__(self, size=0):
+        """The size of a square is crucial for a square,
+        many things depend of it (area computation, etc.)"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """ returns the current square area """
+        return self.__size ** 2
