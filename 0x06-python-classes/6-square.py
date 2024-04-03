de304c8e1e5c6467a4855f843fed74fb66c1131d
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-02 18:26:06 -0700

    7th task

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
 
     def my_print(self):
         """Prints the square with '#'"""
-        if self.__size == 0:
+        if self.size == 0:
             print()
             return
         for _ in range(self.__position[1]):
