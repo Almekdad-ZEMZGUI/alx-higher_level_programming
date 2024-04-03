590376fbaf7bab346ec42a0787d08048eb4f57d7
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-02 17:34:16 -0700

    3rd task

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,9 +7,8 @@ class Square:
     def __init__(self, size=0):
         """The size of a square is crucial for a square,
         many things depend of it (area computation, etc.)"""
-        try:
-            if type(size) is not int:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
