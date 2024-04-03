93961907a6b95af15839b65d76c15381e3609601
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-02 18:44:45 -0700

    10th task

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" class """
+
+
+class Square:
+    """ class """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method to retrieve the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method to set the size of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ returns the current square area """
+        return self.__size ** 2
+
+    def __lt__(self, other):
+        return self.size < other.size
+
+    def __gt__(self, other):
+        return self.size > other.size
+
+    def __ge__(self, other):
+        return self.size >= other.size
+
+    def __le__(self, other):
+        return self.size <= other.size
+
+    def __ne__(self, other):
+        return self.size != other.size
+
+    def __eq__(self, other):
+        return self.size == other.size
