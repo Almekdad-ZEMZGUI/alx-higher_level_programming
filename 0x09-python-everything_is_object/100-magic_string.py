cf64f8a9a34f438bb04e941ed5e68de387321ccb
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-09 15:14:37 -0700

    task 29

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    for i in range(1, 11):
-        print(i * "BestSchool")
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return "BestSchool" + ", BestSchool" * (magic_string.counter - 1)
