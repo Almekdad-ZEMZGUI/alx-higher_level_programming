c54f50cd97ae193777b95be305f44176bb91e0b9
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-15 13:23:54 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+5-base_geometry module
+with BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    class with a Public instance method: def area(self):
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
