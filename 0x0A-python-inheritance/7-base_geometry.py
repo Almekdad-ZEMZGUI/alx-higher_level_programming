d0ff61bd9ef786c00134ba9ea0b4e83b11ac4c52
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-15 13:43:24 -0700

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+7-base_geometry module
+with BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    class with a Public instance methods
+    area and integer_validator
+    """
+    def area(self):
+        """
+        raises an Exception
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        """
+        if isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
