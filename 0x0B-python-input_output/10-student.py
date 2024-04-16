36b934e0183471f13bcec3d6a59737fae69f4d78
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-16 08:49:54 -0700

    task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+10-student module
+with a Class Student
+"""
+
+
+class Student:
+    """
+    Defines a student by:
+    Public Attributes:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        retrieves a dictionary representation
+        of a Student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            d = {}
+            for att in attrs:
+                if att in self.__dict__.keys():
+                    d[att] = self.__dict__[att]
+            return d
