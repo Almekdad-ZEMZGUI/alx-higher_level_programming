e5c02008a50afa11894c57aeaff0995147fbba7a
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-16 08:47:19 -0700

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+9-student module
+with a Class Student
+"""
+
+
+class Student:
+    """
+    Defines a student by:
+    Public Attributes:
+        first_name
+        last_name
+        age
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        retrieves a dictionary representation
+        of a Student instance
+        """
+        return self.__dict__
