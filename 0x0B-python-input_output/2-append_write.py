b382c8fc1bffa0b19b83a9eb1f5902a124ee483b
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-04-16 07:57:50 -0700

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+2-append_write module
+with a function append_write
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    appends a string at the end of a text file (UTF8)
+    and returns the number of characters added
+    """
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return (f.write(text))
