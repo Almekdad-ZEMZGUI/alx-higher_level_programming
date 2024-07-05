e9160bb1d00c549e46b8555c78f05200210e5037
Almekdad-ZEMZGUI <mekzemzgui@gmail.com>
2024-07-05 15:33:06 -0700

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
 """
- fetches https://alx-intranet.hbtn.io/status
+Fetches https://alx-intranet.hbtn.io/status using urllib
 """
 
 import urllib.request
 
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
+
     with urllib.request.urlopen(url) as response:
-        html = response.read()
+        content = response.read()
+        utf8_content = content.decode('utf-8')
         print('Body response:')
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
+        print('\t- utf8 content: {}'.format(utf8_content))
